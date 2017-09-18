1968419fa575f60e5f69acfe25cccab63fa158a4
jasmine Magdi <jesojolay@yahoo.com>
2017-09-18 22:20:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,11 +22,7 @@ end
 
 # code your #valid_move? method here
 def valid_move?(board, index)
-    if index.between?(0,8) && (board[index] == " " || board[index] == "" || board[index] == nil)
-        return true
-    else
-        return false
-    end
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
