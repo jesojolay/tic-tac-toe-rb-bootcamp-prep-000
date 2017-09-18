61b543a0bad5a4ac54207c9019c0dbce28baecae
jasmine Magdi <jesojolay@yahoo.com>
2017-09-18 23:30:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,4 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
@@ -143,5 +144,5 @@ def play(board)
     until over?(board)
         turn(board)
     end
-    
+
 end
